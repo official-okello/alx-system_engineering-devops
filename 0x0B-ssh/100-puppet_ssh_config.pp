47b3b34e57c18aced4a31a8c41bbf57a6b690565
official-okello <officialokello@gmail.com>
2023-09-24 20:38:19 +0300

    It all works

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# sets up a client SSH configuration file so that...
+# we can connect to a server without using a password
+
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  content => $config
+}
