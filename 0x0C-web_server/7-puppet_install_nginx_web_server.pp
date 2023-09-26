f0f648f4e6256ca834b3acf91298a8e0017d8e90
official-okello <officialokello@gmail.com>
2023-09-26 23:04:47 +0300

    It all works

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+# Install and configure nginx on a server with Puppet
+
+$config = "server {
+	listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+}"
+
+package { 'nginx':  # Installs an Nginx server
+ensure	=> 'installed',
+}
+
+file { 'index.html':
+ensure	=> 'present',
+path	=> '/var/www/html/index.html',
+content	=> 'Hello World!',
+mode	=> '0644'
+}
+
+file { 'server_config':
+ensure	=> 'present',
+path 	=> '/etc/nginx/sites-available/default',
+content => $config
+}
+
+exec { 'service nginx restart':
+path	=> ['/usr/sbin', '/usr/bin']
+}
